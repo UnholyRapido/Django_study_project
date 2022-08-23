027143f7004625532af31ff9b9952615e28d684f
rapido <rapiloco@gmail.com>
2022-08-23 17:03:20 +0500

    fixes

--- myfirstsite/myfirstsite/urls.py
+++ myfirstsite/myfirstsite/urls.py
@@ -1,4 +1,4 @@
-"""myfirstsite_root URL Configuration
+"""myfirstsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
