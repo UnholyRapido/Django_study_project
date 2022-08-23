ebafdb149e51e3efe6b1a5723399e0d7be0484ac
rapido <rapiloco@gmail.com>
2022-08-23 22:27:14 +0500

    первая работа с шаблонами

--- root_myfirstsite/news/views.py
+++ root_myfirstsite/news/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
 
 
 # Create your views here.
 def index(request):
     # print(request)
-    return HttpResponse('Hello world!')
+    news = News.objects.order_by('-created_at')
+    # res = '<h1>Список новостей</h1>'
+    # for item in news:
+    #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>'
+    # return HttpResponse(res)
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})  # для большого контекста можно создать переменную со словарём
 
 def test(requets):
     # name = input('enter your name: ')
