e470bb473b18aeef53ffde6bf315c88bca2f07cb
rapido <rapiloco@gmail.com>
2022-09-10 22:35:13 +0500

    1. подключение статический файлов(css, js и всё такое) 2. начало работы с формой добавления новости 3. причёсывание шаблонов(html часть)

--- root_myfirstsite/myfirstsite/settings.py
+++ root_myfirstsite/myfirstsite/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'myfirstsite/static',
+]
+
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
--- root_myfirstsite/news/views.py
+++ root_myfirstsite/news/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import News, Category
+from .forms import NewsForm
 
 
 # Create your views here.
@@ -16,14 +17,31 @@ def index(request):
     return render(request, 'news/index.html', {'news': news,
                                                'title': 'Список новостей'})  # для большого контекста можно создать переменную со словарём
 
+
 def test(requets):
     # name = input('enter your name: ')
     name = 'хозяйка'
     return HttpResponse(f'<h1>Это тестовая страница, {name}</h1>')
 
+
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
+    # category = Category.objects.get(pk=category_id)
+    category = get_object_or_404(Category, pk=category_id)
     context = {'news': news,
                'category': category}
     return render(request, 'news/category.html', context)
+
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
+
+
+def add_news(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
